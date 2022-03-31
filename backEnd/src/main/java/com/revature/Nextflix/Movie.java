4d2f2dac58cce75fdd88edf73788680bdaa3502a
codyb <cody084@revature.com>
2022-03-31 16:36:41 -0400

    Updated SQL and postgres to build.gradle

--- backEnd/src/main/java/com/revature/Nextflix/Movie.java
+++ backEnd/src/main/java/com/revature/Nextflix/Movie.java
@@ -11,6 +11,12 @@ public class Movie {
     private int movie_id;
     @Column(unique = true, nullable = false)
     private String movie_title;
+    @Column(unique = true, nullable = false)
+    private int movie_time;
+    @Column(unique = true, nullable = false)
+    private String movie_ratings;
+    @Column(unique = true, nullable = false)
+    private int movie_year;
 
     public Movie() {
     }
@@ -18,21 +24,52 @@ public class Movie {
     public Movie(int movie_id, String movie_title){
         this.movie_id = movie_id;
         this.movie_title = movie_title;
+        this.movie_time = movie_time;
+        this.movie_ratings = movie_ratings;
+        this.movie_year = movie_year;
     }
 
-    public int getMovie_id() {
+    public int getmovie_id() {
         return movie_id;
     }
 
-    public void setMovie_id() {
+    public void setmovie_id() {
         this.movie_id = movie_id;
     }
 
-    public String getMovie_title() {
+    public String getmovie_title() {
         return movie_title;
     }
 
-    public void setMovie_title() {
+    public void setmovie_title() {
         this.movie_title = movie_title;
     }
+
+    public int getmovie_time() {
+        return movie_time;
+    }
+
+    public void setmovie_time() {
+        this.movie_time = movie_time;
+    }
+
+    public String getmovie_ratings() {
+        return movie_ratings;
+    }
+
+    public void setmovie_ratings() {
+        this.movie_ratings = movie_ratings;
+    }
+
+    public String getmovie_year() {
+        return movie_year;
+    }
+
+    public void setmovie_year() {
+        this.movie_year = movie_year;
+    }
+
+    @Override
+    public String toString() {
+        return "movies [movie_title=" + movie_title + ", movie_id=" + movie_id + "movie_time" + movie_time + "movie_ratings" + movie_ratings + "movie_year" + movie_year + "]";
 }
--- frontend/src/app/app.component.sql
+++ frontend/src/app/app.component.sql
@@ -1,13 +1,13 @@
-CREATE TABLE "Marvel&D.C" (
-  "MovieId" INT PRIMARY KEY NOT NULL,
-  "MovieName"VARCHAR NOT NULL,
-  "MovieTime" INT NOT NULL,
-  "MovieRatings" VARCHAR NOT NULL,
-  "MovieYear" INT NOT NULL
+CREATE TABLE "movies" (
+  "movie_id" INT PRIMARY KEY NOT NULL,
+  "movie_title"VARCHAR NOT NULL,
+  "movie_time" INT NOT NULL,
+  "movie_ratings" VARCHAR NOT NULL,
+  "movie_year" INT NOT NULL
 );
 
-INSERT INTO "Marvel&D.C" VALUES (1, 'Iron Man', 126, 'PG-13', 2008);
-INSERT INTO "Marvel&D.C" VALUES (2, 'Captain America: The First Avenger', 124, 'PG-13', 2011);
-INSERT INTO "Marvel&D.C" VALUES (3, 'Guardians of the Galaxy', 122, 'PG-13', 2014);
-INSERT INTO "Marvel&D.C" VALUES (4, 'Wonder Woman 1984', 151, 'PG-13', 2020);
-INSERT INTO "Marvel&D.C" VALUES (5, 'Batman v Superman: Dawn of Justice', 151, 'PG-13', 2016);
+INSERT INTO "movies" VALUES (1, 'Iron Man', 126, 'PG-13', 2008);
+INSERT INTO "movies" VALUES (2, 'Captain America: The First Avenger', 124, 'PG-13', 2011);
+INSERT INTO "movies" VALUES (3, 'Guardians of the Galaxy', 122, 'PG-13', 2014);
+INSERT INTO "movies" VALUES (4, 'Wonder Woman 1984', 151, 'PG-13', 2020);
+INSERT INTO "movies" VALUES (5, 'Batman v Superman: Dawn of Justice', 151, 'PG-13', 2016);
