9b4fad4f223aeaddfaf5ec22125f275140bd34ce
codyb <cody084@revature.com>
2022-03-30 23:12:08 -0400

    Updated SQL

---
+++ frontend/src/app/app.component.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "Marvel&D.C" (
+  "MovieId" INT PRIMARY KEY NOT NULL,
+  "MovieName"VARCHAR NOT NULL,
+  "MovieTime" INT NOT NULL,
+  "MovieRatings" VARCHAR NOT NULL,
+  "MovieYear" INT NOT NULL
+);
+
+INSERT INTO "Marvel&D.C" VALUES (1, 'Iron Man', 126, 'PG-13', 2008);
+INSERT INTO "Marvel&D.C" VALUES (2, 'Captain America: The First Avenger', 124, 'PG-13', 2011);
+INSERT INTO "Marvel&D.C" VALUES (3, 'Guardians of the Galaxy', 122, 'PG-13', 2014);
+INSERT INTO "Marvel&D.C" VALUES (4, 'Wonder Woman 1984', 151, 'PG-13', 2020);
+INSERT INTO "Marvel&D.C" VALUES (5, 'Batman v Superman: Dawn of Justice', 151, 'PG-13', 2016);
--- src/main/java/com/revature/Nextflix/NextflixApplication.java
+++ src/main/java/com/revature/Nextflix/NextflixApplication.java
@@ -3,6 +3,54 @@ package com.revature.Nextflix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+class Movies{
+	private int MovieId;
+	private String MovieName;
+	private int MovieTime;
+	private String MovieRatings;
+	private int MovieYear;
+	public Movies(int MovieId, String MovieName, int MovieTime, String MovieRatings, int MovieYear){
+		this.MovieId = MovieId;
+		this.MovieName = MovieName;
+		this.MovieTime = MovieTime;
+		this.MovieRatings = MovieRatings;
+		this.MovieYear = MovieYear;
+	}
+	public Movies{}
+	public int getMovieId() {
+		return MovieId;
+	}
+	public void setMovieId(int MovieId) {
+		this.MovieId = MovieId;
+	}
+	public String getMovieName() {
+		return MovieName;
+	}
+	public void setMovieName(String MovieName) {
+		this.MovieName = MovieName;
+	}
+	public String getMovieRatings() {
+		return MovieRatings;
+	}
+	public void setMovieRatings(String MovieRatings) {this.MovieRatings = MovieRatings;}
+	public int getMovieTime() {
+		return  MovieTime;
+	}
+	public void setMovieTime(int MovieTime) {
+		this.MovieTime = MovieTime;
+	}
+	public int getMovieYear() {
+		return MovieYear;
+	}
+	public void setMovieYear(int MovieYear) {
+		this.MovieYear = MovieYear;
+	}
+	@Override
+	public String toString() {
+		return "Planet [MovieName=" + MovieName + ", MovieId=" + MovieId + "MovieRatings" + MovieRatings + "MovieTime" + MovieTime + "MovieYear" + MovieYear + "]";
+	}
+}
+
 @SpringBootApplication
 public class NextflixApplication {
 
