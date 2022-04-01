da8ee862d743ecaac01e086f26b0394cea6dfb54
Justin Freeman <justin045@revature.net>
2022-04-01 12:33:38 -0500

    added api controller

--- backEnd/src/main/java/com/revature/Nextflix/ApiController.java
+++ backEnd/src/main/java/com/revature/Nextflix/ApiController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-//@RestController
-//public class ApiController {
-//    @Autowired
-//    private RestTemplate restTemplate;
-//
-//    @GetMapping("/api")
-//    public Movie api() {
-//        return restTemplate.getForObject("http://localhost:8081/arists/1", Movie.class);
-//    }
-//}
+@RestController
+public class ApiController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/api")
+    public Movie api() {
+        return restTemplate.getForObject("http://localhost:8081/arists/1", Movie.class);
+    }
+}
